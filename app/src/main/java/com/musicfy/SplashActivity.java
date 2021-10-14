be5f7e7776eadb36c5187f27339ad68532fc6f23
Guilherme Fleck Oliveira <guilhermefleckoliveira@gmail.com>
2021-10-14 20:20:09 -0300

    Estrutura inicial, splash e inicio home

---
+++ app/src/main/java/com/musicfy/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.musicfy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Thread() {
+            public void run() {
+                try {
+                    sleep(1000);
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(intent);
+                }
+                catch(Exception e) {}
+            }
+        }.start();
+    }
+}
