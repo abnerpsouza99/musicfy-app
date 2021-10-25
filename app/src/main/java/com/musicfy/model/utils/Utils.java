3945cf8a72248f84110f7ff5c173fcb6d6c3bbde
abnerpsouza99 <abnertecnicosenac@gmail.com>
2021-10-24 22:46:46 -0300

    Adiciona classe utils

---
+++ app/src/main/java/com/musicfy/model/utils/Utils.java
@@ -0,0 +1,19 @@
+package com.musicfy.model.utils;
+
+public class Utils{
+    static final private String apiKey = "73221755SpueZZEnWOcKdYPAdWGEAYpBv4tRbnNkYoFZQX4a0a7Km7uC";
+    static final private String baseUrl = "https://api.happi.dev/v1/music";
+
+    public Utils(){
+
+    }
+
+    public String getApiKey(){
+        return apiKey;
+    }
+
+    public String getBaseUrl(){
+        return baseUrl;
+    }
+
+}
