00832d6bf69b21a0b17068acce64acd5422e6c56
abnerpsouza99 <abnertecnicosenac@gmail.com>
2021-10-27 22:42:53 -0300

    Ajustando model de track

--- app/src/main/java/com/musicfy/model/Track.java
+++ app/src/main/java/com/musicfy/model/Track.java
@@ -1,22 +1,22 @@
 package com.musicfy.model;
 
 import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import androidx.annotation.RequiresApi;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+
 @NoArgsConstructor
-@AllArgsConstructor
-public class Track {
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class Track implements Parcelable {
 
     private int id;
     private String trackName;
@@ -27,6 +27,102 @@ public class Track {
     private boolean hasLyrics;
     private String apiTrack;
     private String apiLyrics;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public int getBpm() {
+        return bpm;
+    }
+
+    public void setBpm(int bpm) {
+        this.bpm = bpm;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public boolean isHasLyrics() {
+        return hasLyrics;
+    }
+
+    public void setHasLyrics(boolean hasLyrics) {
+        this.hasLyrics = hasLyrics;
+    }
+
+    public String getApiTrack() {
+        return apiTrack;
+    }
+
+    public void setApiTrack(String apiTrack) {
+        this.apiTrack = apiTrack;
+    }
+
+    public String getApiLyrics() {
+        return apiLyrics;
+    }
+
+    public void setApiLyrics(String apiLyrics) {
+        this.apiLyrics = apiLyrics;
+    }
+
+    protected Track(Parcel in) {
+        id = in.readInt();
+        trackName = in.readString();
+        artist = in.readString();
+        album = in.readString();
+        bpm = in.readInt();
+        lang = in.readString();
+        hasLyrics = in.readByte() != 0;
+        apiTrack = in.readString();
+        apiLyrics = in.readString();
+    }
+
+    public static final Creator<Track> CREATOR = new Creator<Track>() {
+        @Override
+        public Track createFromParcel(Parcel in) {
+            return new Track(in);
+        }
+
+        @Override
+        public Track[] newArray(int size) {
+            return new Track[size];
+        }
+    };
 
     @Override
     public String toString() {
@@ -60,4 +156,22 @@ public class Track {
         }
     }
 
+    @Override
+    public int describeContents() {
+       return 0;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.trackName);
+        dest.writeString(this.artist);
+        dest.writeString(this.album);
+        dest.writeInt(this.bpm);
+        dest.writeString(this.lang);
+        dest.writeBoolean(this.hasLyrics);
+        dest.writeString(this.apiTrack);
+        dest.writeString(this.apiLyrics);
+    }
 }
