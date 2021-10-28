21362b4cdad01b1334650a40a38ebefcbc26289c
abnerpsouza99 <abnertecnicosenac@gmail.com>
2021-10-27 22:44:44 -0300

    Adiciona chamada do método que busca músicas para apresentar na HomeActivity

--- app/src/main/java/com/musicfy/HomeActivity.java
+++ app/src/main/java/com/musicfy/HomeActivity.java
@@ -1,14 +1,37 @@
 package com.musicfy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+import com.musicfy.presenter.TrackPresenterImpl;
+import com.musicfy.presenter.TrackPresenterInterface;
+
+public class HomeActivity extends AppCompatActivity implements TrackPresenterInterface.view {
+
+    TrackPresenterInterface.presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        presenter = new TrackPresenterImpl(this);
+        presenter.searchTrack();
+    }
+
+    @Override
+    public void buildRecycler(RecyclerView.Adapter adapter) {
+        RecyclerView rv = findViewById(R.id.rvHome);
+        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
+        rv.setLayoutManager(llm);
+        rv.setAdapter(adapter);
+    }
+
+    @Override
+    public Context getContext() {
+        return this.getApplicationContext();
     }
 }
