9746332b7c3eab698eadeb59d955abd28b12bda3
abnerpsouza99 <abnertecnicosenac@gmail.com>
2021-10-27 22:45:25 -0300

    Comenta código que não está sendo utilizado por enquanto

--- app/src/main/java/com/musicfy/model/utils/DetalheTrackActivity.java
+++ app/src/main/java/com/musicfy/model/utils/DetalheTrackActivity.java
@@ -15,13 +15,13 @@ public class DetalheTrackActivity extends AppCompatActivity {
     protected void onCreate (Bundle savedInstaceState){
         super.onCreate(savedInstaceState);
         setContentView(R.layout.activity_detalhe_track);
-        Intent intent = getIntent();
-        TextView tv = findViewById(R.id.tvIdTrack);
-        int i = intent.getIntExtra("id" , -1);
-        Track track = intent.getParcelableExtra("obj");
-        tv.setText(""+track.toString());
-        tv = findViewById(R.id.artist);
-        tv.setText(track.toString());
+//        Intent intent = getIntent();
+//        TextView tv = findViewById(R.id.tvTrackName);
+//        int i = intent.getIntExtra("id" , -1);
+//        Track track = intent.getParcelableExtra("obj");
+//        tv.setText(track.trackName);
+//        tv = findViewById(R.id.artist);
+//        tv.setText(track.toString());
     }
 
 }
